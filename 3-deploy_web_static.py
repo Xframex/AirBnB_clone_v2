3d90f30d66135e886770f7051e891e2ce4534375
Xframex <loukili.issmael@gmail.com>
2023-12-07 12:26:09 -0800

    final deploy

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Compress web static folder into a tgz file"""
+
+from datetime import datetime
+from fabric.api import *
+from os.path import exists
+
+
+env.user = "ubuntu"
+env.hosts = [
+    '54.236.45.211',
+    '18.214.88.83',
+]
+
+
+def do_pack():
+    """Convert web static folder into a tgz file"""
+    local("mkdir -p versions")
+    date = datetime.now()
+    now = date.strftime("%Y%m%d%H%M%S")
+    file_tgz = "versions/web_static_{}.tgz".format(now)
+    try:
+        local("tar -cvzf {} web_static".format(file_tgz))
+        return (file_tgz)
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """distributes an tgz archive web servers (two servers)"""
+    if exists(archive_path):
+        try:
+            put(archive_path, '/tmp/')
+            file_tgz = archive_path.split('/')[-1]
+            file = file_tgz.split('.')[0]
+            archive_folder = '/data/web_static/releases/{}/'.format(file)
+            run('mkdir -p {}'.format(archive_folder))
+            run('tar -xvzf /tmp/{} -C {}'.format(file_tgz, archive_folder))
+            run('rm /tmp/{}'.format(file_tgz))
+            run('mv {}web_static/* {}'.format(archive_folder, archive_folder))
+            run('rm -rf {}web_static'.format(archive_folder))
+            run('rm -rf /data/web_static/current')
+            run('ln -s {} /data/web_static/current'.format(archive_folder))
+            return True
+        except:
+            return False
+    else:
+        return False
+
+
+def deploy():
+    """ distribute an archive to a web server."""
+    file = do_pack()
+    if file is None:
+        return False
+    return do_deploy(file)
