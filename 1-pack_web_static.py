216b1ad376a6f946717202bbdb60a2b76b04d3a6
Xframex <loukili.issmael@gmail.com>
2023-12-07 11:13:12 -0800

    add a fabric file to get tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""web static folder into a tgz file"""
+
+from datetime import datetime
+from fabric.api import *
+
+
+env.user = "ubuntu"
+
+
+def do_pack():
+    """Compress web static folder into a tgz extension"""
+    local("mkdir -p versions")
+    date = datetime.now()
+    now = date.strftime("%Y%m%d%H%M%S")
+    tgz_file = "versions/web_static_{}.tgz".format(now)
+    try:
+        local("tar -cvzf {} web_static".format(tgz_file))
+        return (tgz_file)
+    except:
+        return None
