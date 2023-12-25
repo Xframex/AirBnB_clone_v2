e12bee581804b963f8674c7841a91daa33c5a35d
Xframex <loukili.issmael@gmail.com>
2023-12-25 11:15:29 -0800

    flask3 -fix2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,7 +2,6 @@
 '''flask script'''
 
 from flask import Flask
-from flask import render_template
 app = Flask(__name__)
 
 
@@ -10,15 +9,18 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text=None):
-        """Displays the return message"""
-        string = text.replace('_', ' ')
-        return 'C {}'.format(string)
+    """Displays the return message"""
+    string = text.replace('_', ' ')
+    return 'C {}'.format(string)
+
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run(host='0.0.0.0', port='5000', debug=True)
